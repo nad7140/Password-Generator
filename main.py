312a5ab1165b77eadf997041f1ed2bb288c831cc
nad7140 <38598960+nad7140@users.noreply.github.com>
2022-12-23 15:02:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 import random
+import string
 
-lower = "abcdefghijklmnopqrstuvwxyz"
-upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-numbers = "0123456789"
-symbols = "~!@#$%^&*()_-+=`./<>]["
+print("Hello, Welcome to Password Generator :)")
+
+length = int(input("Enter the lenght of the Password: "))
+
+lower = string.ascii_lowercase
+upper = string.ascii_lowercase
+numbers = string.digits
+symbols = string.punctuation
 
 mix_str = lower+upper+numbers+symbols
 
-password = "".join(random.sample
-                (mix_str,18)) #here if you need increase of length of a password u can increase
+rand = random.sample(mix_str,length)
+
+password = "".join(rand)
+
 print("Password: "+ password)
